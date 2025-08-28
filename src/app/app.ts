9653136fbbfb901d0b7705d3df87b3b14c3b721a
Adam2475 <adamnatanpassaretti@gmail.com>
2025-08-28 07:58:35 +0200

    replacing swiper not working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { SwiperModule } from 'swiper/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, About, Projects, SwiperModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/projects/projects.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SwiperModule } from 'swiper/angular';
+
+@Component({
+  selector: 'app-projects',
+  imports: [SwiperModule],
+  templateUrl: './projects.html',
+  styleUrl: './projects.css'
+})
+export class Projects {
+
+}
